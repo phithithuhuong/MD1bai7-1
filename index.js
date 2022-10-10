2791a4819b96ed813a3aadfda43873edfb473078
phithithuhuong <thu872315@gmail.com>
2022-10-10 16:10:05 +0700

    md1 bài 7-1

---
+++ index.js
@@ -0,0 +1,9 @@
+let a = parseInt(prompt("nhập điểm vật lý"))
+// prompt là chuỗi -- muốn đổi sang số dùng parseInt
+let b = parseInt(prompt("nhập điểm hóa học"))
+let c = parseInt(prompt("nhập điểm sinh học"))
+let tong = (a + b + c)
+let tb = tong / 3
+//console. log : hiển thị kết quả trong ngoăc
+console.log(tong)
+console.log(tb)
